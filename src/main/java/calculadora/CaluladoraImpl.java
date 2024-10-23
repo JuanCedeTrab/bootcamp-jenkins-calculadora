2cd3b0dd85caf470b5ee1cc83374549dfe9fe328
B17657 <B17657@E20003179.banxico.org.mx>
2024-10-23 17:25:39 -0600

    Cambio de tipo de calculo para sumar restar multiplecar y dividir

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -4,7 +4,15 @@ public class CaluladoraImpl implements Calculadora {
 	
 	@Override
 	public int calcular(int i1, char op, int i2) {
-            return i1 + i2;
+            int res;
+            res = switch (op) {
+                case '+' -> i1+i2;
+                case '-' -> i1-i2;
+                case '*' -> i1*i2;
+                case '/' -> i1/i2;
+                default -> 0;
+            };
+            return res;
 	}
 
 }
